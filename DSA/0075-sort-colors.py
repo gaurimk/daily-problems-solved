9b9088a9476419fa1c4d7eb9f783362b612e02b4
Gauri kutakole <gaurikutakole2003@gmail.com>
2026-09-14 22:36:37 +0530

    Move LeetCode 567 solution to DSA

--- DSA/0075-sort-colors.py
+++ DSA/0075-sort-colors.py
@@ -14,16 +14,3 @@ class Solution:
                 nums[mid], nums[high] = nums[high], nums[mid]
                 high -= 1
                 
-
-class Solution:
-    def wiggleSort(self, nums: List[int]) -> None:
-        arr = sorted(nums)
-        n = len(nums)
-        mid = (n + 1) // 2
-        small = arr[:mid][::-1]
-        large = arr[mid:][::-1]
-        for i in range(n):
-            if i % 2 == 0:
-                nums[i] = small[i//2]
-            else:
-                nums[i] = large[i//2]
